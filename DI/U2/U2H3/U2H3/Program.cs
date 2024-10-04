375e6ec36031f3a48c2c1875cc261f34557591f2
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-10-04 17:34:29 +0200

    .

---
+++ DI/U2/U2H3/U2H3/Program.cs
@@ -0,0 +1,55 @@
+﻿
+internal class Figuras
+{
+    protected double b {  get; set; }
+    protected double h {  get; set; }
+
+    public Figuras(double b, double h)
+    {
+        this.b = b;
+        this.h = h;
+    }
+    
+
+}
+
+class rectangulo : Figuras
+{
+    public rectangulo(double b, double h) : base(b, h)
+    {
+    }
+
+    public double Area()
+    {
+        return b * h;
+    }
+}
+
+class triangulo : Figuras
+{
+    public triangulo(double b, double h) : base(b, h)
+    {
+    }
+
+    public double Area()
+    {
+        return b * h / 2;
+    }
+}
+
+class  Rombo 
+{
+    
+}
+
+class Cuadrado : Figuras
+{
+    public Cuadrado(double b, double h) : base(b, h)
+    {
+    }
+
+    public double Area()
+    {
+        return b * h;
+    }
+}
