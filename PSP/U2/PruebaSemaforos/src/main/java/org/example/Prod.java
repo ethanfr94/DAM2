3130ac96e41180b14596816a94117c1627feedca
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-11-06 19:23:13 +0100

    .

---
+++ PSP/U2/PruebaSemaforos/src/main/java/org/example/Prod.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.concurrent.Semaphore;
+
+public class Prod extends Thread {
+
+        private Cola cola;
+        private int n;
+        Semaphore prod;
+        Semaphore cons;
+        Semaphore mutex;
+
+        public Prod (Cola c, int n, Semaphore prod, Semaphore cons, Semaphore mutex){
+            cola = c;
+            this.n =n;
+            this.prod = prod;
+            this.cons = cons;
+            this.mutex = mutex;
+        }
+
+        public void run() {
+            for (int i = 0; i < 20; i++) {
+                try {
+                    prod.acquire();
+                    mutex.acquire();
+                    cola.put(n); //pone el numero
+                    System.out.println("Productor : " + n + ", produce: " + n + "-----------------------------------");
+                    mutex.release();
+                    cons.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+}
