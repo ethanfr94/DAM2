299e528d72254be2fbfe315a8787f4f81caaa28d
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-12-17 17:44:10 +0100

    .

--- AD/Ud3/Ejercicios/Ud3Hoja2/src/Main.java
+++ AD/Ud3/Ejercicios/Ud3Hoja2/src/Main.java
@@ -56,6 +56,7 @@ public class Main {
                             }
                             System.out.println();
                             lee.close();
+                            lee.close();
                         } catch (IOException e) {
                             System.out.println("Error al leer el archivo");
                         } catch (Exception e) {
