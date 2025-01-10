15eff673bff83125e187c89660029acf37f6a458
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2025-01-10 17:30:02 +0100

    .

--- PMDM/Ud2/Ejercicios/DescuentosAppBase/app/src/main/java/com/example/descuentosapp/views/HomeView.kt
+++ PMDM/Ud2/Ejercicios/DescuentosAppBase/app/src/main/java/com/example/descuentosapp/views/HomeView.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.descuentosapp.components.MainButton
 import com.example.descuentosapp.components.MainTextField
@@ -43,7 +44,6 @@ fun HomeView() {
         ContentHomeView(it)
     }
 }
-
 @Composable
 fun ContentHomeView(paddingValues: PaddingValues) {
     val context= LocalContext.current
