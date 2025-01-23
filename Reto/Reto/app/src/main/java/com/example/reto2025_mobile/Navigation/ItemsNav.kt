9068892938e78bab35b2cbc09e798b749b8c0cec
ethanfr94 <152906243+ethanfr94@users.noreply.github.com>
2025-01-23 13:35:01 +0100

    .

--- Reto/Reto/app/src/main/java/com/example/reto2025_mobile/Navigation/ItemsNav.kt
+++ Reto/Reto/app/src/main/java/com/example/reto2025_mobile/Navigation/ItemsNav.kt
@@ -3,13 +3,13 @@ package com.example.reto2025_mobile.Navigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.reto2025_mobile.R
 
 sealed class ItemsNav (
         val icono: ImageVector,
@@ -23,9 +23,8 @@ sealed class ItemsNav (
     object Item_bottom_nav_home:ItemsNav(
         Icons.Filled.Home,"Home", "home")
 
-
     object Item_bottom_nav_misActividades:ItemsNav(
-        Icons.Filled.Info,"MisActividades", "misActividades")
+        Icons.Default.Star,"Mis Acts", "misActividades")
 
     object Item_bottom_nav_perfil:ItemsNav(
         Icons.Filled.Person,"Perfil", "perfil")
