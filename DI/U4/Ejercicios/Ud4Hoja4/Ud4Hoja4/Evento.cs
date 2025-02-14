26a0e04fa7cbf0542aaadffad3ffd4737bb4979b
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2025-02-14 16:19:29 +0100

    .

---
+++ DI/U4/Ejercicios/Ud4Hoja4/Ud4Hoja4/Evento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ud4Hoja4
+{
+    public enum TipoEventoEnum
+    {
+        Todos,
+        Menor10,
+        Mayor16,
+        Mayor18
+    }
+    public class Evento
+    {
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Hora { get; set; }
+        public int Aforo { get; set; }
+        public TipoEventoEnum Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Seguridad { get; set; }
+        public bool Bar { get; set; }
+        public bool Montaje { get; set; }
+        public bool Sanitarios { get; set; }
+        public bool Promotor { get; set; }
+
+        
+
+    }
+}
