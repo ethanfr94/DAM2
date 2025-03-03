9612b2aafdeb651bbae00da6577b0f1e18954a65
ethanfr94 <152906243+ethanfr94@users.noreply.github.com>
2025-03-03 20:32:17 +0100

    .

---
+++ DI/Tienda/Tienda/FormEstudio.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Tienda
+{
+    partial class FormEstudio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgEstudios = new System.Windows.Forms.DataGridView();
+            this.txtFundacion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEstudios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(470, 339);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(208, 20);
+            this.txtPais.TabIndex = 29;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(12, 394);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(619, 56);
+            this.txtDescripcion.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(467, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Pais";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Fundacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Descripcion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Nombre*";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(14, 339);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(208, 20);
+            this.txtNombre.TabIndex = 23;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(828, 427);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 22;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Location = new System.Drawing.Point(747, 427);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 21;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(666, 427);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 20;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgEstudios
+            // 
+            this.dgEstudios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgEstudios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEstudios.Location = new System.Drawing.Point(12, 12);
+            this.dgEstudios.MultiSelect = false;
+            this.dgEstudios.Name = "dgEstudios";
+            this.dgEstudios.ReadOnly = true;
+            this.dgEstudios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgEstudios.Size = new System.Drawing.Size(891, 289);
+            this.dgEstudios.TabIndex = 18;
+            this.dgEstudios.SelectionChanged += new System.EventHandler(this.dgEstudios_SelectionChanged);
+            // 
+            // txtFundacion
+            // 
+            this.txtFundacion.Location = new System.Drawing.Point(242, 339);
+            this.txtFundacion.Name = "txtFundacion";
+            this.txtFundacion.Size = new System.Drawing.Size(208, 20);
+            this.txtFundacion.TabIndex = 30;
+            // 
+            // FormEstudio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 462);
+            this.Controls.Add(this.txtFundacion);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgEstudios);
+            this.Name = "FormEstudio";
+            this.Text = "FormEstudio";
+            ((System.ComponentModel.ISupportInitialize)(this.dgEstudios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgEstudios;
+        private System.Windows.Forms.TextBox txtFundacion;
+    }
+}
---
+++ DI/Tienda/Tienda/FormEstudio.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tienda
+{
+    public partial class FormEstudio : Form
+    {
+
+        TiendaVideojuegosEntities db = new TiendaVideojuegosEntities();
+        List<Estudio> estudios;
+
+        public FormEstudio()
+        {
+            InitializeComponent();
+            cargaDatos();
+        }
+
+        private void cargaDatos()
+        {
+            estudios = db.Estudios.ToList();
+            dgEstudios.DataSource = estudios;
+        }
+
+        public void limpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtFundacion.Text = "";
+            txtPais.Text = "";
+            txtDescripcion.Text = "";
+        }
+
+        public void cargarCampos(Estudio estudio)
+        {
+            txtNombre.Text = estudio.nombre;
+            txtFundacion.Text = estudio.fundacion.ToString();
+            txtPais.Text = estudio.pais;
+            txtDescripcion.Text = estudio.descripcion;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Estudio estudio = new Estudio();
+
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Por favor llena los campos requeridos");
+                return;
+            }
+
+            estudio.nombre = txtNombre.Text;
+            estudio.fundacion = Convert.ToInt32(txtFundacion.Text);
+            estudio.pais = txtPais.Text;
+            estudio.descripcion = txtDescripcion.Text;
+
+            db.Estudios.Add(estudio);
+            db.SaveChanges();
+
+            cargaDatos();
+            limpiarCampos();
+        }
+
+        private void dgEstudios_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgEstudios.SelectedRows.Count > 0)
+            {
+                btnEditar.Enabled = true;
+                btnEliminar.Enabled = true;
+
+                int index = dgEstudios.SelectedRows[0].Index;
+                Estudio estudio = estudios[index];
+                cargarCampos(estudio);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgEstudios.SelectedRows.Count > 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("¿Estás seguro de eliminar este estudio?", "Eliminar", MessageBoxButtons.YesNo);
+                if(dialogResult == DialogResult.Yes) {
+                    int id = Convert.ToInt32(dgEstudios.SelectedRows[0].Cells[0].Value);
+                    Estudio estudio = db.Estudios.Where(v => v.id == id).FirstOrDefault();
+                    if (estudio != null)
+                    {
+                        db.Estudios.Remove(estudio);
+                        db.SaveChanges();
+                        cargaDatos();
+                    }
+                    else
+                    {
+                        MessageBox.Show("El estudio no existe");
+                        return;
+                    }
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona un videojuego");
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("¿Estás seguro de editar este estudio?", "Editar", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                int id = Convert.ToInt32(dgEstudios.SelectedRows[0].Cells[0].Value);
+                Estudio estudio = db.Estudios.Where(v => v.id == id).FirstOrDefault();
+                if (estudio != null)
+                {
+                    if (string.IsNullOrEmpty(txtNombre.Text))
+                    {
+                        MessageBox.Show("Por favor llena los campos requeridos");
+                        return;
+                    }
+
+                    estudio.nombre = txtNombre.Text;
+                    estudio.fundacion = Convert.ToInt32(txtFundacion.Text);
+                    estudio.pais = txtPais.Text;
+                    estudio.descripcion = txtDescripcion.Text;
+
+                    db.SaveChanges();
+                    cargaDatos();
+                    limpiarCampos();
+                }
+                else
+                {
+                    MessageBox.Show("El estudio no existe");
+                    return;
+                }
+            }
+        }
+    }
+}
